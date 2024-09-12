3c23761ecacefcafef0662ebff052524f81ff60e
Tamjid Makhlukat Borno <tamjidborno2009@gmail.com>
2024-09-12 09:40:40 +0600

    first commit

--- novel_site/settings.py
+++ novel_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,8 +152,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key')
